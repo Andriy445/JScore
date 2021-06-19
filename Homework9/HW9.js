831a48b3e6852d32b4a95df0677218f3ec0bf273
Andriy <Shushniak96@gmail.com>
2021-06-19 11:29:32 +0300

    first

---
+++ Homework9/HW9.js
@@ -0,0 +1,23 @@
+// task 1
+// $("a[href^='https://']").attr('target', '_blank');
+
+//task 2
+// $('h2.head').css('background', 'green').find(".inner").css("fontSize","35px");
+
+// task 3
+
+// $("div:first").prependTo($("h3:first"));
+// $("div:last").prependTo($("h3:last"));
+
+//taks 4
+
+// $("input").on("change",function(){
+//     if ($(':checkbox:checked').length == 3){
+//         $("input").attr('disabled','disabled');
+//     }
+// });
+
+
+
+
+
