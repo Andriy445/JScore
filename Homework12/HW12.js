a8ef871c734030c11fb149794803242d039023dd
Andriy <Shushniak96@gmail.com>
2021-07-04 14:15:05 +0300

    first

---
+++ Homework12/HW12.js
@@ -0,0 +1,16 @@
+// task 1
+
+
+// import {
+//     createServer
+// } from 'http';
+// import {
+//     Server
+// } from 'node-static';
+// const file = new Server('.');
+
+// function accept(req, res) {
+//     file.serve(req, res);
+// }
+// createServer(accept).listen(8080);
+// console.log("Server running on port 8080");
